fbdb1603b73c722942c01099cec061b0c2803636
Lexaxd123 <xddatuxd@gmail.com>
2026-09-10 21:39:20 +0800

    Deploy cleaned configuration and routing files

--- app/Config/App.php
+++ app/Config/App.php
@@ -1,12 +1,32 @@
-// Dynamic Base URL Configuration
-public string $baseURL = 'http://localhost/tb32/public/';
+<?php
 
-public function __construct()
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class App extends BaseConfig
 {
-    parent::__construct();
+    public string $baseURL = DYNAMIC_BASE_URL;
 
-    if (isset($_SERVER['HTTP_HOST'])) {
-        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://';
-        $this->baseURL = $protocol . $_SERVER['HTTP_HOST'] . '/';
-    }
+    public array $allowedHostnames = [];
+
+    public string $indexPage = '';
+
+    public string $uriProtocol = 'REQUEST_URI';
+
+    public string $defaultLocale = 'en';
+
+    public bool $negotiateLocale = false;
+
+    public array $supportedLocales = ['en'];
+
+    public string $appTimezone = 'UTC';
+
+    public string $charset = 'UTF-8';
+
+    public bool $forceGlobalSecureRequests = false;
+
+    public array $proxyIPs = [];
+
+    public bool $CSPEnabled = false;
 }
--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -77,3 +77,13 @@ defined('EXIT_USER_INPUT')     || define('EXIT_USER_INPUT', 7);     // invalid u
 defined('EXIT_DATABASE')       || define('EXIT_DATABASE', 8);       // database error
 defined('EXIT__AUTO_MIN')      || define('EXIT__AUTO_MIN', 9);      // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      || define('EXIT__AUTO_MAX', 125);    // highest automatically-assigned error code
+/*
+ | --------------------------------------------------------------------
+ | Dynamic Base URL Detection
+ | --------------------------------------------------------------------
+ */
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https://' : 'http://';
+$host     = $_SERVER['HTTP_HOST'] ?? 'localhost';
+$script   = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+
+defined('DYNAMIC_BASE_URL') || define('DYNAMIC_BASE_URL', $protocol . $host . $script);
